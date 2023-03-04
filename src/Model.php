a8b1b3d06efaa04986a8672fa64beaaeb9504e90
Pader <ypnow@163.com>
2023-03-05 02:50:27 +0800

    Model basic implement.

---
+++ src/Model.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wind\Db;
+
+use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+use JsonSerializable;
+use Traversable;
+use Wind\Db\ModelQuery;
+
+/**
+ * Database Model
+ */
+class Model implements ArrayAccess, IteratorAggregate, JsonSerializable
+{
+
+    const CONNECTION = null;
+    const TABLE = null;
+    const PRIMARY_KEY = 'id';
+
+    private $stored = false;
+
+    private $dirtyAttributes = [];
+    private $changedAttributes = [];
+    private $attributes = [];
+
+    public function offsetExists($name): bool {
+        return isset($this->attributes[$name]) || isset($this->dirtyAttributes[$name]);
+    }
+
+    public function offsetGet($name): mixed {
+        return $this->dirtyAttributes[$name] ?? $this->attributes[$name];
+    }
+
+    public function offsetSet($name, $value): void {
+        $this->__set($name, $value);
+    }
+
+    public function offsetUnset($name): void {
+        $this->__set($name, null);
+    }
+
+    public function getIterator(): Traversable {
+        return new ArrayIterator($this->getCurrentArray());
+    }
+
+    public function jsonSerialize(): mixed {
+        return $this->getCurrentArray();
+    }
+
+    public function getCurrentArray()
+    {
+        return array_merge($this->attributes, $this->dirtyAttributes);
+    }
+
+
+    public static function query()
+    {
+        return ModelQuery::create(static::class, static::CONNECTION);
+    }
+
+    public static function table()
+    {
+        return static::TABLE ?: self::uncamelize(substr(strrchr(static::class, '\\'), 1));
+    }
+
+    /**
+     * 获得 Model 所在数据库的原始连接
+     * @return \Wind\Db\Connection
+     */
+    public static function connection()
+    {
+        return static::CONNECTION !== null ? Db::connection(static::CONNECTION) : Db::connection();
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+    * 驼峰命名转下划线命名
+    *
+    * @param string $camelCaps
+    * @return string
+    */
+    private static function uncamelize($camelCaps) {
+       return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $camelCaps));
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->dirtyAttributes)) {
+            return $this->dirtyAttributes[$name];
+        } elseif (array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
+        } else {
+            throw new \Exception(sprintf('Undefined property: %s::$%s', static::class, $name));
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $this->dirtyAttributes[$name] = $value;
+    }
+
+}
---
+++ src/ModelQuery.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Wind\Db;
+
+use Amp\Promise;
+
+use function Amp\call;
+
+/**
+ * Query Builder for Model
+ */
+class ModelQuery extends QueryBuilder
+{
+
+    protected $modelClass;
+
+    /**
+     * @return self
+     */
+    public static function create($modelClass, $connection)
+    {
+        $connection = $connection !== null ? Db::connection($connection) : Db::connection();
+        $query = (new self($connection))->from($modelClass::table());
+        $query->modelClass = $modelClass;
+        return $query;
+    }
+
+	/**
+	 * 查询一条数据出来
+     *
+	 * @param array $params
+	 * @return Promise<Model|null>
+	 */
+	public function fetchOne(array $params=[]): Promise {
+		return call(function() use ($params) {
+            $data = yield $this->connection->fetchOne($this->buildSelect(), $params);
+            if ($data) {
+                return $this->instanceModel($data);
+            } else {
+                return null;
+            }
+		});
+	}
+
+	/**
+	 * 查询出全部数据
+	 *
+	 * @param string $sql
+	 * @return Promise<array>
+	 */
+	public function fetchAll(array $params=[]): Promise {
+		return call(function() use ($params) {
+			$result = yield $this->query($this->buildSelect(), $params);
+            $indexBy = $this->builder['index_by'] ?? null;
+
+			$rows = [];
+
+			while (yield $result->advance()) {
+				$row = $result->getCurrent();
+                $model = $this->instanceModel($row);
+				if (!$indexBy) {
+                    $rows[] = $model;
+                } else {
+                    if (!isset($row[$indexBy])) {
+                        throw new DbException("Undefined indexBy key '{$indexBy}'.");
+                    }
+                    $rows[$row[$indexBy]] = $model;
+                }
+			}
+
+            return $rows;
+		});
+	}
+
+    /**
+     * @return Model
+     */
+    private function instanceModel($data)
+    {
+        return new $this->modelClass($data);
+    }
+
+}
